26fa4fa13cd0e3a7156db978ba0c5663d33f5d11
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-03-21 17:01:23 +0900

    Sidebarコンポーネントから作成しよう

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import styles from "./Sidebar.module.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className={styles.sidebar}>
+      <div className="header">
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+      <div className={styles.notes}>
+        <div className={styles.item}>
+          <div className={styles.title}>
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容です。</p>
+          <small>最後の修正日XX/XX</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
